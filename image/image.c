bc4470162741a1a3a4e8c7d17f4910a083bfb5fd
Mateusz <mateusz.podstawka23@gmail.com>
2024-03-24 22:29:29 +0100

    image project 1 update

--- image/image.c
+++ image/image.c
@@ -2,19 +2,36 @@
 
 int main(int argc, char* argv[])
 {
+    // arguments check
     if (argc != 2)
     {
         printf("Program usage: ./image <file_name>");
         return 1;
     }
 
-     FILE* image = fopen(argv[1], "r");
+    FILE* image = fopen(argv[1], "rb");
 
-     if (!image)
-     {
-        printf("Cannot open this file");
-        return 1;
-     }
+    // check if file exists
+    if (!image)
+    {
+    printf("Cannot open this file");
+    return 1;
+    }
+
+    //check file format
+    unsigned char marker[2];
+    fread(marker, sizeof(unsigned char), 2 , image);
+    if (marker[0] != 0xFF || marker[1] != 0xD8)
+    {
+    printf("Wrong file format.\n");
+    fclose(image);
+    return 1;
+    }
+
+    while(1)
+    {
+        
+    }
 
      
 }
